b0515dd028bf5c2cf1f12e16ca45098300c0d591
FabiRami94 <fabioerv_1994@hotmail.com>
2024-01-01 21:12:59 -0500

    programas por props

--- CLIENT/src/components/ProgramCard/ProgramCard.jsx
+++ CLIENT/src/components/ProgramCard/ProgramCard.jsx
@@ -2,11 +2,12 @@
 import { NavLink } from "react-router-dom";
 import style from "./ProgramCard.module.css"
 
-const ProgramCard = () => {
+const ProgramCard = ({imageUrl}) => {
     return(
         <>
             <NavLink to={'/photoshop'}>
-                <div className={style.container}>              
+                <h1>{console.log(imageUrl)}</h1>
+                <div className={style.container} style={{backgroundImage: `url(${imageUrl})`}}>              
                 </div>
             </NavLink>
         </>
--- CLIENT/src/components/ProgramCards/ProgramCards.jsx
+++ CLIENT/src/components/ProgramCards/ProgramCards.jsx
@@ -2,9 +2,21 @@
 import ProgramCard from "../ProgramCard/ProgramCard.jsx";
 
 const ProgramCards = () => {
+
+    const noCodeList = [
+        'https://i.ibb.co/Np7WgFj/Imagen-Figma.png',
+        'https://i.ibb.co/dK0gTJn/Imagen-Word-Press.png',
+        'https://i.ibb.co/m0XpGkC/Imagen-Photoshop.png',
+        'https://i.ibb.co/TLtJgXr/Imagen-Illustrator.png'
+    ]
+
     return(
         <> 
-            <ProgramCard></ProgramCard> 
+            {
+                noCodeList.map((noCode, index) => (
+                    <ProgramCard key={index} imageUrl={noCode}></ProgramCard> 
+                ))
+            }
         </>
     )
 };
