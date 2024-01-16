3b99c8335c6520ccc31527b2d9f1328e520f5dbd
FabiRami94 <fabioerv_1994@hotmail.com>
2024-01-15 21:26:08 -0500

    rompecabezas

--- CLIENT/src/assets/imagesIllustratorData.js
+++ CLIENT/src/assets/imagesIllustratorData.js
@@ -4,6 +4,7 @@ const imagesIllustratorData = [
     'https://i.ibb.co/d4tNnsd/Logo-Word-Press.png',
     'https://i.ibb.co/2s8560B/Logo-Photoshop.png',
     'https://i.ibb.co/n6TGF9n/Logo-Illustrator.png',
+    'https://i.ibb.co/tJCW6cL/Recurso-3.png',
 ];
 
 export default imagesIllustratorData;
--- CLIENT/src/views/IllustratorPage/IllustratorPage.jsx
+++ CLIENT/src/views/IllustratorPage/IllustratorPage.jsx
@@ -21,14 +21,24 @@ const IllustratorPage = () => {
                 <img className={styles.logo} src="https://i.ibb.co/n6TGF9n/Logo-Illustrator.png" alt="Logo Illustrator" />
                 <button onClick={handleReturn}>BACK</button>
             </div> 
+            <h1>TRABAJO PROPIO</h1>
             <div className={styles.containerMaterial}>
                 <h2>ICONOS</h2> 
                 <Icons></Icons>
             </div>
-            <h1>TRABAJO PROPIO</h1>
             {/* <div className={styles.containerMaterial}>  
                 <h2 style={{color: 'white'}}>WORK IN PROCESS...</h2>    
             </div>  */}
+            <div className={styles.containerMaterial}>
+                <h2>ROMPECABEZAS DE TECNOLOGÍAS</h2>  
+                <ImagesIllustrator imagePosition = {[4]}></ImagesIllustrator>
+                <div className={styles.divNotes}>  
+                    <img className={styles.notesImage} src="https://i.ibb.co/p2T0FFC/Recurso-2.png" />
+                    <span>
+                        Se buscaba darle expresión a la unión de diferentes tecnologías en un rompecabezas.
+                    </span>
+                </div>        
+            </div>
             <div className={styles.containerMaterial}>
                 <h2>PROGRAMAS NO CODE</h2>  
                 <ImagesIllustrator imagePosition = {[0,1,2,3]}></ImagesIllustrator>
