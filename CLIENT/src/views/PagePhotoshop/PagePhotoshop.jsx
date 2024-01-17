52ad743908b61466a3235e20d20ccf12e0190b72
FabiRami94 <fabioerv_1994@hotmail.com>
2024-01-17 08:07:12 -0500

    title and subtatle photoshop

--- CLIENT/src/views/PagePhotoshop/PagePhotoshop.jsx
+++ CLIENT/src/views/PagePhotoshop/PagePhotoshop.jsx
@@ -18,12 +18,12 @@ const PagePhotoshop = () => {
                 <img className={styles.logo} src="https://i.ibb.co/ZGF3qS3/Logo-propio-Blanco.png" alt="Logo Blanco" />
                 <img className={styles.logo} src="https://i.ibb.co/2s8560B/Logo-Photoshop.png" alt="Logo Photoshop" />
             </div> 
-            <h1>TRABAJO PROPIO</h1>
+            <h1 className={styles.title}>TRABAJO PROPIO</h1>
             {/* <div className={styles.containerMaterial}>  
                 <h2 style={{color: 'white'}}>WORK IN PROCESS...</h2> 
             </div>  */}
             <div className={styles.containerMaterial}>
-                <h2>PROGRAMAS NO CODE</h2>  
+                <h2 className={styles.subtitle}>PROGRAMAS NO CODE</h2>  
                 <ImagesPhotoshop imagePosition = {[0,1,2,3]}></ImagesPhotoshop>
                 <div className={styles.divNotes}>  
                     <img className={styles.notesImage} src="https://i.ibb.co/p2T0FFC/Recurso-2.png" />
