b4e33c15dbd385b7a970e6177d17ae52fe3142cf
FabiRami94 <fabioerv_1994@hotmail.com>
2024-01-16 20:36:04 -0500

    arreglo de illustrator

--- CLIENT/src/views/IllustratorPage/IllustratorPage.jsx
+++ CLIENT/src/views/IllustratorPage/IllustratorPage.jsx
@@ -14,23 +14,19 @@ const IllustratorPage = () => {
 
     return(
         
-        <>  
-            <div className={styles.centerContent}>
-                <button onClick={handleReturn}>BACK</button>
+        <div>  
+            <div>
+                <button className={styles.principalButton} onClick={handleReturn}>BACK</button>
                 <img className={styles.logo} src="https://i.ibb.co/ZGF3qS3/Logo-propio-Blanco.png" alt="Logo Blanco" />
                 <img className={styles.logo} src="https://i.ibb.co/n6TGF9n/Logo-Illustrator.png" alt="Logo Illustrator" />
-                <button onClick={handleReturn}>BACK</button>
             </div> 
-            <h1>TRABAJO PROPIO</h1>
+            <h1 className={styles.title}>TRABAJO PROPIO</h1>
             <div className={styles.containerMaterial}>
-                <h2>ICONOS</h2> 
+                <h2 className={styles.subtitle}>ICONOS</h2> 
                 <Icons></Icons>
             </div>
-            {/* <div className={styles.containerMaterial}>  
-                <h2 style={{color: 'white'}}>WORK IN PROCESS...</h2>    
-            </div>  */}
             <div className={styles.containerMaterial}>
-                <h2>ROMPECABEZAS DE TECNOLOGÍAS</h2>  
+                <h2 className={styles.subtitle}>ROMPECABEZAS DE TECNOLOGÍAS</h2>  
                 <div>
                     <img src="https://i.ibb.co/TLZ5C3L/Rompecabezas.png"/>
                 </div>
@@ -42,7 +38,7 @@ const IllustratorPage = () => {
                 </div>        
             </div>
             <div className={styles.containerMaterial}>
-                <h2>PROGRAMAS NO CODE</h2>  
+                <h2 className={styles.subtitle}>PROGRAMAS NO CODE</h2>  
                 <ImagesIllustrator imagePosition = {[0,1,2,3]}></ImagesIllustrator>
                 <div className={styles.divNotes}>  
                     <img className={styles.notesImage} src="https://i.ibb.co/p2T0FFC/Recurso-2.png" />
@@ -51,7 +47,7 @@ const IllustratorPage = () => {
                     </span>
                 </div>        
             </div>
-        </>
+        </div>
     )
 };
 
